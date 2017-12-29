7a284e5ebe4bc95d1cdc9c73be92e871a1982609
grzebyk44 <grzebyk44@gmail.com>
2017-12-29 19:55:06 +0100

    'driving_threads_feature'

--- tests/Browser/ThreatsTest.php
+++ tests/Browser/ThreatsTest.php
@@ -10,11 +10,25 @@ class ThreatsTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
-    public function testAUserCanBrowseThreads()
+    public function testAUserCanBrowseAllThreads()
     {
         $this->browse(function (Browser $browser) {
+        	$thread = factory('App\Thread')->create();
+
             $browser->visit('/threads')
-                    ->assertStatus(200);
+                    ->assertSee($thread->title);
+
+        });
+    }
+
+    public function testAUserCanBrowseThread()
+    {
+        $this->browse(function (Browser $browser) {
+        	$thread = factory('App\Thread')->create();
+
+            $browser->visit('/threads/'.$thread->id)
+                    ->assertSee($thread->title);
         });
     }
 }
+				
